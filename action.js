e4b8d0c7945b8a06fa3b9a1b4bae69b454dd2807
Cody Gates <cody.gates.89@gmail.com>
2018-04-17 18:37:56 -0600

    add level selector

--- action.js
+++ action.js
@@ -1,11 +1,31 @@
 
-let golfHoles = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
-let numPlayers = 30;
+let numPlayers = 4;
+let course;
+
+loadDoc();
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            course = JSON.parse(this.responseText);
+            console.log(course);
+            let selectTees = course.holes[0].tees;
+            for(let t = 0; t < selectTees.length; t++);
+                $("#teeSelection").append("<option value='" + t + "'>" + selectTees[t].teeName + "</option>");
+
+
+            //createCard();
+        }
+    };
+    xhttp.open("GET", "holes.txt", true);
+    xhttp.send();
+}
 
 
 function createCard(){
-    for(let i = 0; i < golfHoles.length; i++){
-        $(".right").append("<div id='col"+ i +"' class='column'><div class='cHead'>"+ (i + 1) +"</div></div>");
+    for(let i = 0; i < course.holes.length; i++){
+        $(".bottom").append("<div id='row"+ i +"' class='holeRow'><div class='rowHead'>"+ course.holes[i].name +"</div></div>");
 
 
     }
@@ -15,9 +35,9 @@ function createCard(){
 
 function fillCard(){
     for(let p = 1; p <= numPlayers; p++){
-        $(".left").append("<div class='playerLabel' contenteditable='true'>Player " + p + "</div>");
-        for(let h = 0; h < golfHoles.length; h++){
-            $("#col" + h).append("<input class='holeInput' id='p"+ p +"h"+ h +"' type='text'>");
+        $(".top").append("<div class='playerLabel' contenteditable='true'>Player " + p + "</div>");
+        for(let h = 0; h < course.holes.length; h++){
+            $("#row" + h).append("<input class='holeInput' id='p"+ p +"h"+ h +"' type='text'>");
         }
     }
 }
